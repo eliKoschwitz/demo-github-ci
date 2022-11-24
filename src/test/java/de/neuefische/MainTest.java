e0b23b037c3053f32b53c9f9a73e36d6cfcead01
elikoschwitz <e.koschwitz@googmail.com>
2022-11-24 14:56:08 +0100

    Init: Test Upgedatet but still failing

--- src/test/java/de/neuefische/MainTest.java
+++ src/test/java/de/neuefische/MainTest.java
@@ -1,22 +1,20 @@
 package de.neuefische;
 
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
 
     @Test
-    public void addWhen2And5Then7() {
-        // GIVEN
-        int a = 2;
-        int b = 5;
-
-        // WHEN
-        int actual = Main.add(a, b);
-
-        // THEN
-        Assertions.assertEquals(9, actual);
+    void add() {
+        //GIVEN
+        int num1 = 5;
+        int num2 = 6;
+        //WHEN
+        int actual = Main.add(num1, num2);
+        //THEN
+        Assertions.assertEquals(10, actual);
     }
+
 }
